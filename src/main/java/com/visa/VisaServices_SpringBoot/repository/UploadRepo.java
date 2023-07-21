483ee3bf2f0c1d658922671602f36b7753d92abd
atiqbinhabib <101540931+atiqbinhabib@users.noreply.github.com>
2023-07-21 20:58:15 +0600

    Update UploadRepo.java

--- src/main/java/com/visa/VisaServices_SpringBoot/repository/UploadRepo.java
+++ src/main/java/com/visa/VisaServices_SpringBoot/repository/UploadRepo.java
@@ -2,7 +2,10 @@ package com.visa.VisaServices_SpringBoot.repository;
 
 import com.visa.VisaServices_SpringBoot.models.UploadModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UploadRepo extends JpaRepository<UploadModel, Long> {
